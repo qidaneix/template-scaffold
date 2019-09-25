2cb42741a65b8e584c311294ad8446992461fca6
肖金 <xiaojin1992@126.com>
2019-09-25 15:54:57 +0800

    prettier

--- src/baz.js
+++ src/baz.js
@@ -1,7 +1,13 @@
-import { ArrayBi } from "./foo";
+import { ArrayBi } from './foo';
 
 export { ArrayBi };
 
-export function write(str) {
+export function write(str, a, b, c, d, e, f) {
   document.write(str);
+  document.write(a);
+  document.write(b);
+  document.write(c);
+  document.write(d);
+  document.write(e);
+  document.write(f);
 }
