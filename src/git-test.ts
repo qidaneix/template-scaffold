222e97371e5619663be6585eb6d02f3751446bf2
xiaoj <xiaoj@tencent.com>
2020-04-12 22:43:33 +0800

    feat: git commit amend test 2

--- src/git-test.ts
+++ src/git-test.ts
@@ -4,4 +4,8 @@ $(document).on('click', '#app', e => {
   $('#app').text(JSON.stringify(e.target));
   console.log(e.target.tagName);
   // 为什么会落后？
+  // try again
+  // what the hell???
+  // test test 111
+  // test test 222
 });
