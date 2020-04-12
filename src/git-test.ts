d67873276fabff39ffd4b347b2ac5d0b0d9f50f5
xiaoj <xiaoj@tencent.com>
2020-04-12 22:16:22 +0800

    test: test git push

--- src/git-test.ts
+++ src/git-test.ts
@@ -4,29 +4,4 @@ $(document).on('click', '#app', e => {
   $('#app').text(JSON.stringify(e.target));
   console.log(e.target.tagName);
   // 为什么会落后？
-  // try again
-  // what the hell???
-  // test test 111
-  // test test 222
-  // some oper wrong
-  // test test 333
-  // try again!!!
-  // more change
-  // what the hell
-  // do not know 1
-  // do not know 1 amend
-  // go to hell
-  // go to hell 1
-  // go 1
-  // go 1 amend
-  // go 1 amend 2
-  // meng le ya
-  // meng le ya amend
-  // go 2
-  // go 2 amend
-  // go 3
-  // go 3 amend
-  // sb is you
-  // sb is me
-  // smart is you
 });
